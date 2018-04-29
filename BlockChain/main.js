0a00bb8c7232e4f8d076887f05bd9c39db971c11
Alessandro Marques <vyseclown@gmail.com>
2018-04-29 20:04:22 -0300

    Nova atualizacao com a parte três

--- BlockChain/main.js
+++ BlockChain/main.js
@@ -1,66 +1,98 @@
-const SHA256 = require('crypto-js/sha256');
+
+const SHA256 = require("crypto-js/sha256");
+
 class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
     }
-
 }
-class Block{
-    constructor(timestamp, transactions, previousHash = ''){
+
+class Block {
+    constructor(timestamp, transactions, previousHash = '') {
+        this.previousHash = previousHash;
         this.timestamp = timestamp;
-        this.data = transactions;
+        this.transactions = transactions;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
-    calculateHash(){//a assinatura digital
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    calculateHash() {
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
-
-    //é necessario ter uma quantidade de 0 na frente do calculo de hash para provar que realmente foi usado muito poder computacional, complicando também cada vez mais o calculo a ser feito
+    //é necessario ter uma quantidade de 0 na frente do calculo de hash para provar(proof of work que realmente foi usado muito poder computacional, complicando também cada vez mais o calculo a ser feito
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
 
-        console.log("BLOCK MINED: " + this.hash);
+        console.log("Bloco minerado: " + this.hash);
     }
-
 }
+
+
 class Blockchain{
-    constructor(){//criar o primeiro bloco
-        this.chain = [this.createGenesisBlock];
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 5;
     }
 
-    createGenesisBlock(){//primeiro bloco
-        return new Block("01/04/2018", "Genesis Block", "0");
+    createGenesisBlock() {
+        return new Block(Date.parse("2017-01-01"), [], "0");
     }
 
-    getLatestBlock(){//pegar o ultimo bloco
-        return this.chain[this.chain.length-1];
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){//adicionar um novo bloco
-        newBlock.previousHash = this.getLatestBlock().hash;
-        //newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Bloco minerado com sucesso !');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
     }
 
-    isChainValid(){//validar se o hash do ultimo bloco é o que veio para a criação desse novo bloco e se o bloco atual tem o mesmo hash se for feito o mesmo calculo novamente
-        for(let i = 1; i < this.chain.length; i++){
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid() {//validar se o hash do ultimo bloco é o que veio para a criação desse novo bloco e se o bloco atual tem o mesmo hash se for feito o mesmo calculo novamente
+        for (let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i-1];
+            const previousBlock = this.chain[i - 1];
 
-            if(currentBlock.hash !== currentBlock.calculateHash()){
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
 
-            if(currentBlock.previousHash !== previousBlock.hash){
+            if (currentBlock.previousHash !== previousBlock.hash) {
                 return false;
             }
         }
@@ -69,3 +101,16 @@ class Blockchain{
     }
 }
 
+let dicoin = new Blockchain();
+dicoin.createTransaction(new Transaction('address1', 'address2', 100));//seriam os endereços das carteiras publicas
+dicoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+console.log('\n Starting the miner...');
+dicoin.minePendingTransactions('xaviers-address');
+
+console.log('\nBalance of xavier is', dicoin.getBalanceOfAddress('xaviers-address'));
+
+console.log('\n Starting the miner again...');
+dicoin.minePendingTransactions('xaviers-address');
+
+console.log('\nBalance of xavier is', dicoin.getBalanceOfAddress('xaviers-address'));
